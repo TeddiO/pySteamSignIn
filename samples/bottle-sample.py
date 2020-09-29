05f157e5eb0a4fddfc38dda660543a0e74de2514
Teddi <teddi.orange@gmail.com>
2020-09-29 19:28:41 +0100

    General sanity and logging updates
    - Clarify a few comments better
    - Change how RedirectUser gets loaded
    - Added logging to allow users to follow execution
    - Made the bottle sample a bit more explicit in terms of giving back a
      result

--- samples/bottle-sample.py
+++ samples/bottle-sample.py
@@ -1,4 +1,6 @@
-from bottle import run, request, route
+from bottle import run, request, route, sys, os
+# Not ideal, but for the sake of an example
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
 from steamsignin import SteamSignIn
 
 @route('/')
@@ -21,7 +23,15 @@ def process():
 	steamLogin = SteamSignIn()
 	steamID = steamLogin.ValidateResults(returnData)
 
-	print(steamID)
+	print("SteamID returned was: ", steamID)
 
+	if steamID is not False:
+		return "We logged in successfully!<br />SteamID: {0}".format(steamID)
+	else:
+		return "Failed to log in, bad details?"
+
+	# At this point, redirect the user to a friendly URL
+
+	
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug = True, reloader = False)
