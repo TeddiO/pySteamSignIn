d08903f25a2c8973312fd17a7054918f8899a95c
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-08-13 15:28:54 +0200

    lowercase name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="steamSignIn",
+    name="pysteamsignin",
     version="1.0.0",
     author="TeddiO",
     author_email="",
