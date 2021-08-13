454013e6e6bacf8203072ded8300fe82c439d78c
Teddi <me@teddi.dev>
2021-08-13 19:14:56 +0100

    fix license for pypi & update docs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pysteamsignin",
+    name="steamsignin",
     version="1.0.0",
     author="TeddiO",
     author_email="",
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(exclude=('samples')),
